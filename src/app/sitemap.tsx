c1481bb3502ed9ac12cd687cab11bd570577dd17
modori1208 <11772828+modori1208@users.noreply.github.com>
2025-11-03 17:18:05 +0900

    feat: generate sitemap dynamically from menu data

---
+++ src/app/sitemap.tsx
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+
+import { MENU } from '@/constants/menu';
+import { METADATA } from '@/constants/metadata';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const entries = MENU.map((menu) => ({
+    url: `${METADATA.url}${menu.href}`,
+    ...('date' in menu && { lastModified: menu.date }),
+    priority: 0.9,
+  }));
+
+  return [
+    {
+      url: `${METADATA.url}`,
+      changeFrequency: 'yearly',
+      priority: 1.0,
+    },
+    ...entries
+  ];
+}
