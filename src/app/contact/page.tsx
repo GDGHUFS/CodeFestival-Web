8bf2589ab4f8efcadea3da88d627350282717404
modori1208 <11772828+modori1208@users.noreply.github.com>
2025-11-06 16:08:47 +0900

    docs: update 7th festival docs

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,6 +8,9 @@ export default function Page() {
     <Layout className={styles.root}>
       <p>문의 사항은 아래 이메일로 연락 부탁드립니다.</p>
       <a href={`mailto:${METADATA.email}`}>{METADATA.email}</a>
+      <br />
+      <p>제7회 코드페스티벌 문의 카카오톡 오픈채팅방</p>
+      <a href="https://open.kakao.com/o/gJYVpy0h">https://open.kakao.com/o/gJYVpy0h</a>
     </Layout>
   );
 }
